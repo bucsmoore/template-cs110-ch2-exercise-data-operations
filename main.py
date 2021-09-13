a85455a371b9adbdd618d316365343b82cf56c8b
Steven A Moore <stevenallenmoore@gmail.com>
2021-09-13 16:06:07 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+mylist = [1,2,3,4,5]
+#print each item in the list on a seperate line
+
+
+#swap the first and last element of the list
+
+
+#Ask the user for a number and append it to the list, thenm print the list again
